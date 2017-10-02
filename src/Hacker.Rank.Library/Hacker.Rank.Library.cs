71165c97f645797ed14596bd6c9b31bd3faf5d6a
taesoku <benjamin.han@bidenergy.com>
2017-10-02 15:40:00 +1100

    - Hacker Rank Library : updated

---
+++ src/Hacker.Rank.Library/Hacker.Rank.Library.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacker.Rank.Library
+{
+    class HackerRankLibrary
+    {
+
+        // count: return int - counting
+        // find: return object
+        // get: return array || list
+        // is: return bool
+        // max: return int - finding maximum
+        // min: return int - finding minimum
+        // print: return string
+        // run: return void
+
+        public static void Main()
+        {
+            IsValidEmailAddressesRegex.Answer();
+
+        }
+
+    }
+}
---
+++ src/Hacker.Rank.Library/IsValidEmailAddressesRegex.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Hacker.Rank.Library
+{
+    class IsValidEmailAddressesRegex
+    {
+        public static void Answer()
+        {
+            var inputs = new List<string>
+            {
+                "julia@hackerrank.com",
+                "julia_@hackerrank.com",
+                "julia_0@hackerrank.com",
+                "julia0_@hackerrank.com",
+                "julia@gmail.com"
+            };
+            IsValidEmailAddressRegex(5, inputs);
+        }
+
+        public static void IsValidEmailAddressRegex(int n, List<string> inputs)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                
+                var temp = inputs[i].Split('@');
+
+                if (temp.Last() != "hackerrank.com")
+                {
+                    Console.WriteLine("False");
+                    continue;
+                }
+
+                var regexPattern = @"^([a-z]{1,6})+$"; // the first condition
+                var matches = Regex.Match(temp.First(), regexPattern);
+
+                if (matches.Success)
+                {
+                    Console.WriteLine("True");
+                    continue;
+                }
+
+                regexPattern = @"^([a-z]{0,5}[_]{1})+$"; // the second condition
+                matches = Regex.Match(temp.First(), regexPattern);
+                if (matches.Success)
+                {
+                    Console.WriteLine("True");
+                    continue;
+                }
+
+                regexPattern = @"^([a-z]{0,5}[_]{1}[0-9]{1,4})+$"; // the third condition
+                matches = Regex.Match(temp.First(), regexPattern);
+                if (matches.Success)
+                {
+                    Console.WriteLine("True");
+                    continue;
+                }
+
+                Console.WriteLine("False");
+
+            }
+        }
+    }
+}
