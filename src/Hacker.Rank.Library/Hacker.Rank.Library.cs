793ac52a4a7f3f8df1bc682149e9644bdb6117a9
taesoku <benjamin.han@bidenergy.com>
2017-11-28 10:31:49 +1100

    - Print Chocolates Per Day : updated

--- src/Hacker.Rank.Library/Hacker.Rank.Library.cs
+++ src/Hacker.Rank.Library/Hacker.Rank.Library.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Hacker.Rank.Library
 {
     class HackerRankLibrary
@@ -20,9 +15,9 @@ namespace Hacker.Rank.Library
 
         public static void Main()
         {
+            //PrintValidEmailAddressRegexClass.Answer();
             //SaveGifFileNamesClass.Answer();
             SaveHostsCountClass.Answer();
-            //PrintValidEmailAddressRegexClass.Answer();
 
         }
 
