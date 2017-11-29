4bb22ee4f2fe3d35a8ba520659dccef832451835
taesoku <benjamin.han@bidenergy.com>
2017-11-30 08:58:12 +1100

    - Print You Can Sort : added

---
+++ src/Hacker.Rank.Library/PrintYouCanSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hacker.Rank.Library
+{
+    class PrintYouCanSort
+    {
+
+        public static void Answer()
+        {
+            var inputs = new int[2];
+            PrintYouCanSortDictionary(inputs);
+        }
+
+        public static void PrintYouCanSortDictionary(int[] inputs)
+        {
+            var dictionary = new Dictionary<int, int>();
+            foreach (var input in inputs)
+            {
+                if (dictionary.ContainsKey(input))
+                {
+                    var temp = dictionary[input];
+                    dictionary[input] = temp + 1;
+                }
+                else dictionary[input] = 1;
+            }
+            var ordered = dictionary.OrderBy(d => d.Key).OrderBy(d => d.Value);
+            foreach (var o in ordered)
+            {
+                for (var i = 0; i < o.Value; i++) Console.WriteLine(o.Key);
+            }
+        }
+
+    }
+}
