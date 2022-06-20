62103cf29b6a34819950699c1f7a45099adf433c
taesoku <dominic77674@gmail.com>
2022-06-21 06:13:23 +0900

    - Week01PlusMinus

---
+++ Hacker.Rank.3.Months.Preparation.Kit/Hacker.Rank.3.Months.Preparation.Kit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacker.Rank._3.Months.Preparation.Kit
+{
+    class HackerRank3MonthsPreparationKit
+    {
+        static void Main(string[] args)
+        {
+            //Week01PlusMinus.Answer();
+        }
+    }
+}
---
+++ Hacker.Rank.3.Months.Preparation.Kit/Week01.Plus.Minus.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacker.Rank._3.Months.Preparation.Kit
+{
+    class Week01PlusMinus
+    {
+
+        public static void Answer()
+        {
+            var inputs = new List<int> { -4, 3, -9, 0, 4, 1 };
+            PlusMinus(inputs);
+        }
+
+        private static void PlusMinus(List<int> inputs)
+        {
+            var count = inputs.Count();
+            var negative = 0d;
+            var positive = 0d;
+            var zero = 0d;
+            foreach (var a in inputs)
+            {
+                if (a == 0) zero++;
+                else if (a > 0) positive++;
+                else negative++;
+            }
+            Console.WriteLine(string.Format("{0:N6}", positive > 0 ? positive / count : 0));
+            Console.WriteLine(string.Format("{0:N6}", negative > 0 ? negative / count : 0));
+            Console.WriteLine(string.Format("{0:N6}", zero > 0 ? zero / count : 0));
+        }
+
+    }
+}
