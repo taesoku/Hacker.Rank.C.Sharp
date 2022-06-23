c01f66de8ad35e0e15badb07de207f36bca7b14e
taesoku <dominic77674@gmail.com>
2022-06-24 07:57:21 +0900

    - Week01RunCamelCase4

---
+++ Hacker.Rank.3.Months.Preparation.Kit/Week01.Run.Camal.Case.4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Hacker.Rank._3.Months.Preparation.Kit
+{
+    class Week01RunCamelCase4
+    {
+        public static void Answer()
+        {
+
+        }
+
+        public static void RunCamelCase4()
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == null || input.Length == 0) break;
+                var splits = input.Split(';').ToList();
+                var output = splits.Last();
+                if (splits[0] == "S")
+                {
+                    output = new Regex(@"(?<=[^A-Z])(?=[A-Z])", RegexOptions.IgnorePatternWhitespace).Replace(output, " ").ToLower();
+                    if (splits[1] == "M")
+                        output = output.Replace("(", "").Replace(")", "").Trim();
+                }
+                else if (splits[0] == "C")
+                {
+                    output = Regex.Replace(output, @"\b\w", (Match match) => match.ToString().ToUpper());
+                    output = output.Replace(" ", "");
+                    if (splits[1] != "C")
+                        output = char.ToLower(output[0]).ToString() + output.Substring(1);
+                    if (splits[1] == "M")
+                        output = output + "()";
+                }
+
+                Console.WriteLine(output);
+            }
+        }
+
+    }
+}
