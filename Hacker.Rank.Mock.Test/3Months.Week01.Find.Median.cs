e4d51f8c77f915a3c88794bf63e672146ce84b10
taesoku <dominic77674@gmail.com>
2022-07-03 13:17:11 +0900

    - 3 Months Week 01 Find Median

---
+++ Hacker.Rank.Mock.Test/3Months.Week01.Find.Median.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacker.Rank.Mock.Test
+{
+    class _3MonthsWeek01FindMedian
+    {
+        public static void Answer()
+        {
+
+        }
+
+        public static int FindMedian(List<int> inputs)
+        {
+            return 0;
+        }
+    }
+}
