e83ee02e3b32da349d613bea6d90e8632d69c529
taesoku <dominic77674@gmail.com>
2022-07-07 06:43:19 +0900

    - Week03 Print XOR String 3

---
+++ Hacker.Rank.3.Months.Preparation.Kit/Week03.Print.XOR.String.3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacker.Rank._3.Months.Preparation.Kit
+{
+    class Week03PrintXORString3
+    {
+
+        public static void Answer()
+        {
+            var input = "07:05:45PM";
+            PrintXORString3(input, input);
+        }
+
+        private static string PrintXORString3(string s, string t)
+        {
+            var output = new StringBuilder();
+            for (var i = 0; i < s.Length; i++)
+                if (s[i] == t[i]) output.Append("0");
+                else output.Append("1");
+            return output.ToString();
+        }
+
+    }
+}
