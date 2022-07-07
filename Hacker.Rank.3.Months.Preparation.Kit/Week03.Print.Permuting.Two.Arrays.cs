13639e455ae103006dc78b2c3d692fccf6f13eb9
taesoku <dominic77674@gmail.com>
2022-07-08 08:50:21 +0900

    - Week03 Print Permuting Two Arrays

---
+++ Hacker.Rank.3.Months.Preparation.Kit/Week03.Print.Permuting.Two.Arrays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacker.Rank._3.Months.Preparation.Kit
+{
+    class Week03PrintPermutingTwoArrays
+    {
+
+        public static void Answer()
+        {
+            var inputs1 = new List<int> { 1, 2, 3 };
+            var inputs2 = new List<int> { 4, 5, 6 };
+            PrintPermutingTwoArrays(3, inputs1, inputs2);
+        }
+
+        private static string PrintPermutingTwoArrays(int k, List<int> a, List<int> b)
+        {
+            a.Sort();
+            b.Sort();
+            b.Reverse();
+            for (var i = 0; i < a.Count; i++)
+                if (a[i] + b[i] < k) return "NO";
+            return "YES";
+        }
+
+    }
+}
