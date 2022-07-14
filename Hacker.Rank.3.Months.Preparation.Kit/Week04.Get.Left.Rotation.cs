b5830a857b309eb83f41e4d13fb85cfd45b66f54
taesoku <dominic77674@gmail.com>
2022-07-15 06:24:46 +0900

    - Week04 Get Left Rotation

---
+++ Hacker.Rank.3.Months.Preparation.Kit/Week04.Get.Left.Rotation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacker.Rank._3.Months.Preparation.Kit
+{
+    class Week04GetLeftRotation
+    {
+
+        public static void Answer()
+        {
+            var inputs1 = new List<int> { 5, 2, 3, 4, 1 };
+            GetLeftRotation(inputs1, 1);
+        }
+
+        private static List<int> GetLeftRotation(List<int> inputs, int r)
+        {
+            var n = inputs.Count;
+            if (n == r) return inputs;
+            for (var i = 0; i < n; i++)
+                inputs.Add(inputs[i]);
+            return inputs.GetRange(r, n);
+        }
+    }
+}
