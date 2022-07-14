375c59781d2f4cf4add85afaab32ecf40a940abe
taesoku <dominic77674@gmail.com>
2022-07-14 09:54:18 +0900

    - Week04 Print Number Line Jumps

---
+++ Hacker.Rank.3.Months.Preparation.Kit/Week04.Print.Number.Line.Jumps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacker.Rank._3.Months.Preparation.Kit
+{
+    class Week04PrintNumberLineJumps
+    {
+
+        public static void Answer()
+        {
+            PrintNumberLineJumps(1, 2, 3, 4);
+        }
+
+        private static string PrintNumberLineJumps(int x1, int v1, int x2, int v2)
+        {
+            if (v1 <= v2) return "NO";
+            while (x1 <= int.MaxValue && x2 <= int.MaxValue)
+            {
+                if (x1 == x2) return "YES";
+                else
+                {
+                    x1 += v1;
+                    x2 += v2;
+                }
+                if (x1 > x2) return "NO";
+            }
+            return "NO";
+        }
+
+    }
+}
