8f8b9195ad1b179535f2ea8bf4488e0c3596d05c
taesoku <dominic77674@gmail.com>
2022-07-24 07:38:05 +0900

    - Week05 Find Fibonacci Modified

---
+++ Hacker.Rank.Mock.Test/3Months.Week05.Find.Fibonacci.Modified.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacker.Rank.Mock.Test
+{
+    class _3MonthsWeek05FindFibonacciModified
+    {
+        public static void Answer()
+        {
+
+        }
+
+        public static void FindFibonacciModified(int t1, int t2, int n)
+        {
+//def fibonacciModified(t1, t2, n):
+//    i = 2
+//    while i < n:
+//        t1, t2 = t2, t1 + t2*t2
+//        i += 1
+//    return t2
+        }
+    }
+}
