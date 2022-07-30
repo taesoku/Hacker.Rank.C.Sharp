5588a2a740e687d57f81d29555f7188a9d953998
taesoku <dominic77674@gmail.com>
2022-07-31 08:54:57 +0900

    - Week06 Find Palindrome Index

---
+++ Hacker.Rank.Mock.Test/3Months.Week06.Find.Palindrome.Index.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacker.Rank.Mock.Test
+{
+    class _3MonthsWeek06FindPalindromeIndex
+    {
+        public static void Answer()
+        {
+
+        }
+
+        public static int FindPalindromeIndex(string input)
+        {
+            var i = 0;
+            var j = input.Length - 1;
+            while (i < j)
+            {
+                if (input[i] != input[j])
+                {
+                    if (input[i + 1] == input[j] && input[i + 2] == input[j - 1]) return i;
+                    else return j;
+                }
+                i++;
+                j--;
+            }
+            return -1;
+        }
+    }
+}
